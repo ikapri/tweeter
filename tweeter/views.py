7787d6665266b49ae9c048ab6add1753e062c524
Ishaan Kapri <ishaankapri11@gmail.com>
2013-12-09 02:54:09 +0530

    Template added and view modified

--- tweeter/views.py
+++ tweeter/views.py
@@ -1,13 +1,19 @@
-from flask import jsonify
+from flask import render_template
 from tweeter import app
 from mongo_connection import Conn
 import json
 
 @app.route('/')
-def latest_tweets():
+@app.route('/<int:page>')
+def get_tweets(page=None):
 	conn = Conn()
 	data =[]
-	tweets = conn.get_latest_tweets()
+	limit = 10
+	if not page:
+		skip = 0
+	else:
+		skip = 2*(page-1)		
+	tweets = conn.get_tweets(skip,limit)
 	for t in tweets:
 		data.append(t)
-	return json.dumps(data)
+	return render_template('index.html',tweets=data)
