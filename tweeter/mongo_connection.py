92700b1a289605de6b319bd8315218acbbb125b8
Ishaan <ishaan@ishaan-Inspiron-1545.(none)>
2013-12-09 02:52:24 +0530

    Template added and view modified

--- tweeter/mongo_connection.py
+++ tweeter/mongo_connection.py
@@ -5,5 +5,5 @@ class Conn():
 	def __init__(self,host=None,port=None):
 		self.conn = MongoClient(host=host,port=port)
 		self.db = self.conn[config.DB]
-	def get_latest_tweets(self):
-		return self.db.tweet.find(fields={'_id':False}).sort('created_at',-1).limit(20)	
+	def get_tweets(self,skip,limit):
+		return self.db.tweet.find(fields={'_id':False}).sort('created_at',-1).skip(skip).limit(limit)	
