7439ec2a2dbebe524989fc7c6308a90b9d5b7c01
Ishaan Kapri <ishaankapri11@gmail.com>
2013-12-10 04:25:48 +0530

    end point added for realtime streaming

--- tweeter/__init__.py
+++ tweeter/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_sockets import Sockets
 
 app = Flask(__name__)
+sockets = Sockets(app)
 app.debug = True
 import views
--- tweeter/views.py
+++ tweeter/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
-from tweeter import app
+from tweeter import app,sockets
 from mongo_connection import Conn
 import json
+import pika
 
 @app.route('/')
 @app.route('/<int:page>')
@@ -9,11 +10,27 @@ def get_tweets(page=None):
 	conn = Conn()
 	data =[]
 	limit = 10
-	if not page:
-		skip = 0
-	else:
+	skip = 0
+	if page:
 		skip = 2*(page-1)		
 	tweets = conn.get_tweets(skip,limit)
 	for t in tweets:
 		data.append(t)
 	return render_template('index.html',tweets=data)
+
+@app.route('/realtime')
+def get_realtime_tweets():
+	return render_template('realtime.html')
+
+@sockets.route('/pull')
+def pull_tweets(ws):
+	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+	def tweet_received(channel,method,property,body):
+		ws.send(body)
+	channel = connection.channel()
+	channel.exchange_declare(exchange='tweets',type='fanout')
+	q=channel.queue_declare(exclusive=True)
+	q_name = q.method.queue
+	channel.queue_bind(exchange='tweets',queue=q_name)
+	channel.basic_consume(tweet_received,queue=q_name,no_ack=True)	
+	channel.start_consuming()
