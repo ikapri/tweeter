b8225e102dbaf2f25d50f6c30b297ae8b1b639e2
Ishaan Kapri <ishaankapri11@gmail.com>
2013-12-09 14:49:49 +0530

    Tweets from different topics stored in seperate collections

--- tweeter/celery_tasks.py
+++ tweeter/celery_tasks.py
@@ -8,14 +8,14 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 
 c = Conn()
 db = c.db
-
+collection = c.collection
 @celery_app.task
 def store_tweets(tweet):
     try:
     	created_at = tweet['created_at'].split(' ')
     	date_string = created_at[1]+' '+created_at[2]+' '+created_at[3]+' '+created_at[5]
     	epoch = datetime.strptime(date_string,"%b %d %H:%M:%S %Y").strftime("%s")
-    	db.tweet.insert({'id':tweet['id_str'],'rt_count':tweet['retweet_count'],'text':tweet['text'],'username':tweet['user']['name'],'screen_name':tweet['user']['screen_name'],'profile_image_url':tweet['user']['profile_image_url'],'created_at':epoch})
+    	collection.insert({'id':tweet['id_str'],'text':tweet['text'],'username':tweet['user']['name'],'screen_name':tweet['user']['screen_name'],'profile_image_url':tweet['user']['profile_image_url'],'created_at':epoch})
     except Exception as e:
     	print e
 
--- tweeter/mongo_connection.py
+++ tweeter/mongo_connection.py
@@ -5,5 +5,6 @@ class Conn():
 	def __init__(self,host=None,port=None):
 		self.conn = MongoClient(host=host,port=port)
 		self.db = self.conn[config.DB]
+		self.collection = self.db[config.FORM_DATA['track'].replace('#','').lower()]
 	def get_tweets(self,skip,limit):
-		return self.db.tweet.find(fields={'_id':False}).sort('created_at',-1).skip(skip).limit(limit)	
+		return self.collection.find(fields={'_id':False}).sort('created_at',-1).skip(skip).limit(limit)	
